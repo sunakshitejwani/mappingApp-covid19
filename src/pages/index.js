3a13c83a2a5340e2f7a7e10037a6d321868b8e4a
sunakshitejwani <sunakshitejwani@gmail.com>
2022-04-01 00:59:14 +0530

    convert coutries data to geographic data

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,6 +63,30 @@ const MapEffect = ({ markerRef }) => {
       const { data = [] } = response;
       console.log(data);
 
+      const hasData = Array.isArray(data) && data.length > 0;
+
+      if (!hasData) return;
+
+      const geoJson = {
+        type: "FeatureCollection",
+        features: data.map((country = {}) => {
+          const { countryInfo = {} } = country;
+          const { lat, long: lng } = countryInfo;
+          return {
+            type: "Feature",
+            properties: {
+              ...country
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [lng, lat]
+            }
+          };
+        })
+      };
+
+      console.log(geoJson);
+
       // const popup = L.popup({
       //   maxWidth: 800,
       // });
