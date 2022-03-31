970d6d8319705b2391ded65b76a29f42a0a94d01
sunakshitejwani <sunakshitejwani@gmail.com>
2022-04-01 01:05:06 +0530

    adding coronavirus data to map

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ import Snippet from "components/Snippet";
 import gatsby_astronaut from "assets/images/gatsby-astronaut.jpg";
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -86,6 +86,58 @@ const MapEffect = ({ markerRef }) => {
       };
 
       console.log(geoJson);
+
+      const geoJsonLayers = new L.GeoJSON(geoJson, {
+        pointToLayer: (feature = {}, latlng) => {
+          const { properties = {} } = feature;
+          let updatedFormatted;
+          let casesString;
+      
+          const {
+            country,
+            updated,
+            cases,
+            deaths,
+            recovered
+          } = properties
+      
+          casesString = `${cases}`;
+      
+          if ( cases > 1000 ) {
+            casesString = `${casesString.slice(0, -3)}k+`
+          }
+      
+          if ( updated ) {
+            updatedFormatted = new Date(updated).toLocaleString();
+          }
+      
+          const html = `
+            <span class="icon-marker">
+              <span class="icon-marker-tooltip">
+                <h2>${country}</h2>
+                <ul>
+                  <li><strong>Confirmed:</strong> ${cases}</li>
+                  <li><strong>Deaths:</strong> ${deaths}</li>
+                  <li><strong>Recovered:</strong> ${recovered}</li>
+                  <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+                </ul>
+              </span>
+              ${ casesString }
+            </span>
+          `;
+      
+          return L.marker( latlng, {
+            icon: L.divIcon({
+              className: 'icon',
+              html
+            }),
+            riseOnHover: true
+          });
+        }
+      });
+
+      geoJsonLayers.addTo(map);
+      
 
       // const popup = L.popup({
       //   maxWidth: 800,
